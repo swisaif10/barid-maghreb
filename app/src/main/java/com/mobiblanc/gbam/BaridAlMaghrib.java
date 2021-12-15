edcc66c1eddedcf9f90790a328c15e9b9363c20e
ERRATBAOUI ACHRAF <erratbaoui@mobiblanc.com>
2021-12-15 14:35:03 +0100

    Added: FCM Notification & Flurry

--- app/src/main/java/com/mobiblanc/gbam/BaridAlMaghrib.java
+++ app/src/main/java/com/mobiblanc/gbam/BaridAlMaghrib.java
@@ -4,12 +4,16 @@ import android.app.Application;
 
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.flurry.android.FlurryAgent;
+
 public class BaridAlMaghrib extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-
+        new FlurryAgent.Builder()
+                .withLogEnabled(true)
+                .build(this, "WDMMP7FXCD5JZDM8WKT4");
     }
 }
--- app/src/main/java/com/mobiblanc/gbam/firebase/FirebaseService.java
+++ app/src/main/java/com/mobiblanc/gbam/firebase/FirebaseService.java
@@ -1,14 +1,25 @@
 package com.mobiblanc.gbam.firebase;
 
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.mobiblanc.gbam.R;
 import com.mobiblanc.gbam.datamanager.sharedpref.PreferenceManager;
 import com.mobiblanc.gbam.utilities.Constants;
+import com.mobiblanc.gbam.views.splashscreen.SplashScreenActivity;
 
 public class FirebaseService extends FirebaseMessagingService {
 
@@ -18,7 +29,45 @@ public class FirebaseService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        Log.d(TAG, "MESSAGE RECEIVED");
+        if (remoteMessage.getNotification() != null) {
+            handleNotification(remoteMessage.getNotification());
+        }
+    }
 
+    private void handleNotification(RemoteMessage.Notification notification) {
+        Intent intent = new Intent(this, SplashScreenActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        @SuppressLint("UnspecifiedImmutableFlag")
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String title = notification.getTitle();
+        String body = notification.getBody();
+
+        String channelId = getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.logo)
+                        .setContentTitle(title)
+                        .setContentText(body)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 
     @Override
